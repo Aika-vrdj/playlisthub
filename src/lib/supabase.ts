6a134ecbb1dcdd21bf4575b10a43900e03e4ceb8
Aika-vrdj <151849141+Aika-vrdj@users.noreply.github.com>
2025-01-23 22:01:48 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://qqrytredypjcqzrlzzez.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxcnl0cmVkeXBqY3F6cmx6emV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2ODQ4MTYsImV4cCI6MjA1MzI2MDgxNn0.W1cPvk3x3zp66UtaVINBRHIOXw2gAbVdysL228s_-5E';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
